8df9ed1a0fe1ecd0af6d6d291189e2f2584a34a5
Aadith <aadithaadhi33@gmail.com>
2026-09-11 16:41:12 +0530

    initial project setup

---
+++ main.py
@@ -0,0 +1,59 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-3.6-flash",
+    contents = "Coffee on a table with  chatgpt on the computer "
+)
+
+print(response.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
