1586e8b7fc66ef2abd25eb432c92ed210a5815bd
Aadith <aadithaadhi33@gmail.com>
2026-09-23 17:43:58 +0530

    ...

--- ranker.py
+++ ranker.py
@@ -1,30 +1,22 @@
-import re
-
-
-def tokenize(text):
-    return set(re.findall(r"\b\w+\b", text.lower()))
-
-
-def calculate_score(context, image):
-    context_words = tokenize(context)
-    alt_words = tokenize(image.alt)
-
-    score = len(context_words.intersection(alt_words))
-
-    return score
+def score_image(context_words, image):
+    alt_words = set(image.alt.lower().split())
+    matching_words = context_words.intersection(alt_words)
+    return len(matching_words)
 
 
 def rank_images(context, images):
-    ranked_images = []
+    context_words = set(context.lower().split())
 
+    scored = []
     for image in images:
-        score = calculate_score(context, image)
+        score = score_image(context_words, image)
+        scored.append((score, image))
 
-        ranked_images.append((score, image))
+    # highest score first
+    scored.sort(key=lambda pair: pair[0], reverse=True)
 
-    ranked_images.sort(
-        key=lambda item: item[0],
-        reverse=True
-    )
+    ranked_images = []
+    for score, image in scored:
+        ranked_images.append(image)
 
-    return [image for score, image in ranked_images]
+    return ranked_images
