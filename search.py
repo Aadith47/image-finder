8796262e45882e8ede80419ed4a0a3683b48a84b
Aadith <aadithaadhi33@gmail.com>
2026-09-26 17:56:45 +0530

    ...

--- search.py
+++ search.py
@@ -1,13 +1,14 @@
 def search_from_queries(queries, search_fn, per_query=5):
     all_images = []
-    seen_urls = set()
+    seen_keys = set()
 
     for query in queries:
         results = search_fn(query, per_page=per_query)
 
         for image in results:
-            if image.image_url not in seen_urls:
-                seen_urls.add(image.image_url)
+            key = (image.source, str(image.id))
+            if key not in seen_keys:
+                seen_keys.add(key)
                 all_images.append(image)
 
     return all_images
